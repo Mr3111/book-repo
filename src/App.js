17c63635562caf3bf054b68e0a2684f6522ce0f2
mishrari <rishab.mishra@cdk.com>
2021-01-24 22:52:30 +0530

    removed unwanted import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import './App.css'
 import {connect} from 'react-redux'
-import {fetchArticleDetails} from "./actions";
 import {createBrowserHistory} from 'history'
 import {Route, Router, Switch} from "react-router";
 import Footer from "./components/Footer";
